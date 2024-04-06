99855a057b6f3d983f643b8439bbf293d7cd9c7b
malithwijay <malith.nipun@gmail.com>
2024-04-06 19:21:26 +0530

    commenting the generate data

--- main.js
+++ main.js
@@ -78,27 +78,27 @@ app.listen(port, () => {
 
 //generate and insert random data into the database
 
-const districts = ['Ampara', 'Anuradhapura', 'Bhadulla', 'Batticoloa', 'Colombo', 'Galle', 'Gampaha', 'Hambantota', 'Jaffna', 'Kalutara', 'Kandy'];
+// const districts = ['Ampara', 'Anuradhapura', 'Bhadulla', 'Batticoloa', 'Colombo', 'Galle', 'Gampaha', 'Hambantota', 'Jaffna', 'Kalutara', 'Kandy'];
 
-const generateRandomData = () => {
-    const temperature = (Math.random() * (35 - 20) + 20).toFixed(2);
-    const humidity = (Math.random() * (100 - 60) + 60).toFixed(2);
-    const windSpeed = (Math.random() * (1020 - 1000) + 1000).toFixed(2);
-    const dist = districts[Math.floor(Math.random() * districts.length)]; // Randomly select a province
+// const generateRandomData = () => {
+//     const temperature = (Math.random() * (35 - 20) + 20).toFixed(2);
+//     const humidity = (Math.random() * (100 - 60) + 60).toFixed(2);
+//     const windSpeed = (Math.random() * (1020 - 1000) + 1000).toFixed(2);
+//     const dist = districts[Math.floor(Math.random() * districts.length)]; // Randomly select a province
 
-    const query = `UPDATE weather SET humidity=${humidity}, temp=${temperature}, wind=${windSpeed} WHERE district = ${dist}`;
-    connection.query(query, [temperature, humidity, airPressure, province], (err, result) => {
-        if (err) {
-            console.error('Error inserting data:', err);
-        } else {
-            console.log('Weather data inserted successfully');
-        }
-    });
-};
+//     const query = `UPDATE weather SET humidity=${humidity}, temp=${temperature}, wind=${windSpeed} WHERE district = ${dist}`;
+//     connection.query(query, [temperature, humidity, airPressure, province], (err, result) => {
+//         if (err) {
+//             console.error('Error inserting data:', err);
+//         } else {
+//             console.log('Weather data inserted successfully');
+//         }
+//     });
+// };
 
-// Generate data every 5 minutes
-setInterval(generateRandomData, 5 * 60 * 1000);
+// // Generate data every 5 minutes
+// setInterval(generateRandomData, 5 * 60 * 1000);
 
-// Generate initial data immediately
-generateRandomData();
+// // Generate initial data immediately
+// generateRandomData();
 
